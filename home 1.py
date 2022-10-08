b45642e4d1017319cc84ed11abf3343d4851a51b
npax1 <prakh55555@gmail.com>
2022-10-08 12:22:17 +0300

    home work

---
+++ home 1.py
@@ -0,0 +1,19 @@
+first=int(input("first number"))
+second=int(input("first number"))
+if first == 7:
+    print("Good bye!")
+elif first<=0:
+    print("Number is negative")
+elif first >= 0:
+    print("Number is positive")
+elif first == 0:
+    print("Number is equal to zero")
+
+if second == 7:
+    print("Good bye!")
+elif second<=0 :
+    print("Number is negative")
+elif second >= 0:
+    print("Number is positive")
+elif second == 0:
+    print("Number is equal to zero")
