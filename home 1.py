2932d4d01ba2ec29768009e0ef8c6b09df738363
npax1 <prakh55555@gmail.com>
2022-10-08 12:31:08 +0300

    home work

--- home 1.py
+++ home 1.py
@@ -1,19 +1,11 @@
-first=int(input("first number"))
-second=int(input("first number"))
-if first == 7:
-    print("Good bye!")
-elif first<=0:
-    print("Number is negative")
-elif first >= 0:
-    print("Number is positive")
-elif first == 0:
-    print("Number is equal to zero")
-
-if second == 7:
-    print("Good bye!")
-elif second<=0 :
-    print("Number is negative")
-elif second >= 0:
-    print("Number is positive")
-elif second == 0:
-    print("Number is equal to zero")
+sum=0
+while True:
+    num=int(input())
+    if num != 7:
+        if num<=0:
+            print("Number is negative")
+        if num >= 0:
+            print("Number is positive")
+        sum+=num
+    else:
+        print("sum=",sum)
